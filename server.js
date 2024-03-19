4539db98131b2b1fa787e64350cfac34f60f0f87
AppTrailblazer <rabit.cr.257@gmail.com>
2024-03-19 00:02:07 -0500

    changing port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Respond to GET requests with JSON data containing an image and text
 app.get('/', (req, res) => {
