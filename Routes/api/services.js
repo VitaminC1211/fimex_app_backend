30a0b52044b1ef2fb9688ad83e310757dc973c3a
AppTrailblazer <rabit.cr.257@gmail.com>
2024-03-25 11:43:02 -0500

    update

--- Routes/api/services.js
+++ Routes/api/services.js
@@ -12,9 +12,10 @@ router.post('/createProduct', (req, res) => {
     //     images: req.body.inner_image.images
     // })
     req.body.forEach((item) => {
-        console.log(item.inner_image);
+        console.log(item.inner_info);
+        
     });
-    res.send(req.body.inner_image)
+    res.send(req.body.inner_info)
     // newService.save()
     //     .then(user => res.send("1"))
     //     .catch(err => console.log(err));
