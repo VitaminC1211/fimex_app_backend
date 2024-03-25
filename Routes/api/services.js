0fe097fcf0c8e8a2cf3920469dbff287e0d3521d
AppTrailblazer <rabit.cr.257@gmail.com>
2024-03-25 17:52:08 -0500

    update

--- Routes/api/services.js
+++ Routes/api/services.js
@@ -6,79 +6,34 @@ const Service = require('../../models/Service');
 
 //@router POST api/services/createProduct
 router.post('/createProduct', (req, res) => {
+    req.body.forEach((product) => {
+        const newService = new Service({
+            id: product.id,
+            image: product.image,
+            inner_image: product.inner_image.map((innerImage) => ({
+                id: innerImage.id,
+                images: innerImage.images,
+                description: innerImage.description,
+                inner_info: innerImage.inner_info.map((info) => ({
+                    id: info.id,
+                    text: info.text,
+                    phone_inner: info.phone.map((phone_inner) => ({
+                        id: phone_inner.id,
+                        phone_type: phone_inner.phone_type,
+                        country_price: phone_inner.country_price.map((country_price) => ({
+                            id: country_price.id,
+                            price: country_price.price
+                        }))
+                    }))
+                }))
+            }))
+        });
 
-    let img;
-    let images;
-    let description;
-    let text;
-    let phone_type;
-    let prices;
-    let imgid;
-    let innerImgId;
-    let innerInfoId;
-    let innerPhoneId;
-    let countryId;
-    
-    req.body.forEach((item) => {
-        imgid = item.id
-        img = item.image
-        //values for inner image
-        item.inner_image.forEach((inner_item) => {
-            innerImgId = inner_item.id 
-            images = inner_item.images
-            description = inner_item.description
-            //valuse for inner info
-            inner_item.inner_info.forEach((inner_phone) => {
-                innerInfoId= inner_phone.id
-                text = inner_phone.text
-                //values for phone inner
-                inner_phone.phone.forEach((countryindex) => {
-                    innerPhoneId = countryindex.id
-                    phone_type = countryindex.phone_type
-                    //values for country_price
-                    countryindex.country_price.forEach((price) => {
-                        countryId = price.id
-                        prices = price.price
-                    })
-                })
-            })
-        })
+        newService.save()
+            .then(product => console.log(product))
+            .catch(err => console.error(err));
     });
-    console.log("ImageChecking"+img);
-    const newService = new Service({
-        id: imgid,
-        image: img,
-        inner_image: [
-            {
-                id: innerImgId,
-                images: images,
-                description: description,
-                inner_info: [
-                    {
-                        id: innerInfoId,
-                        text: text,
-                        phone_inner: [
-                            {
-                                id: innerPhoneId,
-                                phone_type: phone_type,
-                                country_price: [
-                                    {
-                                        id: countryId,
-                                        price: prices
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    });
-    // Save the new Service instance to the database
-    newService.save()
-        .then(service => res.json(service))
-        .catch(err => console.error(err));
-
+    res.send('Products saved successfully');
 })
 
 //@router POST api/services/getAll
